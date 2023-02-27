6c1bc168a3f19fb80cd78b1c50ac0956ae2bd678
Rachel Li <rachel20010813@gmail.com>
2023-02-27 15:40:08 -0500

    add stats to tooltip

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,18 @@ import { useState, useEffect } from 'react';
 import { Tooltip } from 'react-tooltip';
 import 'react-tooltip/dist/react-tooltip.css'
 
+export interface CountryStats {
+  name: string;
+  population: number;
+  capital: string;
+  gini_index: number;
+}
+
 export default function App() {
   const [tooltipContent, setTooltipContent] = useState('test');
   const [tooltipPosition, setTooltipPosition] = useState({ x: 0, y: 0 });
+  const [countryStats, setCountryStats] =
+    useState<CountryStats>({ name: '', population: 0, capital: '', gini_index: 0 });
 
   function onMouseUpdate(e: MouseEvent) {
     setTooltipPosition({ x: e.pageX, y: e.pageY });
@@ -21,8 +30,21 @@ export default function App() {
     };
   }, []);
 
-  return <ThemeProvider>
-    <MapChart setTooltipContent={setTooltipContent} setTooltipPosition={setTooltipPosition} />
-    <Tooltip id='map-tooltip' position={tooltipPosition}>{tooltipContent}</Tooltip>
-  </ThemeProvider>;
+  return (
+    <ThemeProvider>
+      <MapChart
+        setTooltipContent={setTooltipContent}
+        setTooltipPosition={setTooltipPosition}
+        setCountryStats={setCountryStats}
+      />
+      <Tooltip id="map-tooltip" position={tooltipPosition}>
+        {countryStats.name}
+        <ul>
+          <li>population: {countryStats.population}</li>
+          <li>capital: {countryStats.capital}</li>
+          <li>gini index: {countryStats.gini_index}</li>
+        </ul>
+      </Tooltip>
+    </ThemeProvider>
+  );
 }
