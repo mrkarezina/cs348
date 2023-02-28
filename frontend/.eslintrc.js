8808ebca0c63fe7c2246e6af19c7c5ad2c981f74
Elijah Lopez <elijahllopezz@gmail.com>
2023-02-27 21:37:14 -0500

    Fix tooltip and refactor and add country click

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -5,5 +5,12 @@ module.exports = {
   },
   rules: {
     'react/react-in-jsx-scope': 'off',
+    'linebreak-style': 'off',
+    'indent': 'off',
+    'max-len': ['error', 200],
+    'react/jsx-closing-tag-location': 'off',
+    'react/jsx-indent-props': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'jsx-quotes': ['error', 'prefer-single']
   },
 };
