64b4c7f66ae24b7ea912b17f7d72d06554a1de23
Elijah Lopez <elijahllopezz@gmail.com>
2023-03-01 21:04:45 -0500

    Select country on search and better tooltip float

--- frontend/src/apiCalls.tsx
+++ frontend/src/apiCalls.tsx
@@ -9,7 +9,7 @@ export interface CountryStats {
 export const fetchCountryInfo = async ({ code }: { code: string }): Promise<CountryStats> => {
     const info = await fetch(`https://restcountries.com/v2/alpha/${code}`, { cache: 'force-cache' }).then(res => res.json()).then(data => data);
     return {
-        code,
+        code: info.alpha3Code,
         name: info.name,
         population: info.population,
         capital: info.capital,
