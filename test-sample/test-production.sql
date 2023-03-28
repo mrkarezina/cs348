7c30121b4e103890767abe7fc8c9aaed251e8e18
rachel133199 <z963li@uwaterloo.ca>
2023-03-28 00:00:44 -0400

    sample product queries and output

---
+++ test-sample/test-production.sql
@@ -0,0 +1,8 @@
+-- First display all tables by running "\dt"
+
+SELECT value FROM area WHERE country_id = 'CAN';
+
+select value from population where country_id='CAN';
+select value from area where country_id='CAN';
+select value from gini_index where country_id='CAN';
+select value from education_expenditure where country_id='CAN';
