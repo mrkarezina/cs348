036d6f13962ba7f302baf4267dc3fc8760f68aee
Abdullah Hadi <a3hadi@protonmail.com>
2023-03-30 21:01:31 -0400

    removed redundant code

--- backend/project/__init__.py
+++ backend/project/__init__.py
@@ -64,14 +64,12 @@ def country_stats():
                             WHERE country_id = '{country_id}';")
             data[stat] = cursor.fetchone()
             resp = make_response({country_id: data})
-            resp.headers['Access-Control-Allow-Origin'] = '*'
-            response = (resp, 201)
     except psycopg2.Error as e:
         error = jsonify(f"{type(e).__module__.removesuffix('.errors')}:{type(e).__name__}: {str(e).rstrip()}")
         response = (error, 400)
 
     cursor.close()
-    return response
+    return resp, 201
 
 # TODO: refactor all code below
 
