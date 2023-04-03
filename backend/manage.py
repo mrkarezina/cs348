b49a953bbdd84d88cfc6940d0299899b36ff8913
mrkarezina <mrk.arezina@gmail.com>
2023-04-03 19:46:15 -0400

    Update manage.py script to check for tables

--- backend/manage.py
+++ backend/manage.py
@@ -1,91 +1,102 @@
 from flask.cli import FlaskGroup
 import os
+import psycopg2
 
-from project import app, connection
+from project import app
 
 cli = FlaskGroup(app)
+class StatTableManager:
+    def __init__(self):
+        self.conn = psycopg2.connect(user="user",
+                              password="password",
+                              host="db",
+                              port="5432",
+                              database="world_factbook")
 
+    def run_all_scripts(self):
+        self.create_static_tables()
+        self.create_stat_tables()
+        self.populate_static_tables()
+        self.populate_stat_tables()
+        self.create_recent_stat_tables()
+        self.populate_recent_stat_tables()
+        print("Tables created and populated successfully.")
 
-@cli.command("create_static_tables")
-def create_static_tables():
-    cursor = connection.cursor()
-    cursor.execute(open("utilities/drop_static_tables.sql", "r").read())
-    cursor.execute(open("utilities/static_schema.sql", "r").read())
-    connection.commit()
+    def run_all_scripts_with_check(self):
+        if self.check_tables_exist():
+            print("Tables already exist. Skipping script execution.")
+        else:
+            print("Tables do not exist. Executing scripts ...")
+            self.run_all_scripts()
 
+    def check_tables_exist(self):
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT EXISTS(SELECT * FROM pg_tables WHERE schemaname = 'public')")
+        tables_exist = cursor.fetchone()[0]
+        return tables_exist
 
+    def create_static_tables(self):
+        cursor = self.conn.cursor()
+        cursor.execute(open("utilities/drop_static_tables.sql", "r").read())
+        cursor.execute(open("utilities/static_schema.sql", "r").read())
+        self.conn.commit()
 
-# TODO: create separate function for dropping tables
-@cli.command("create_stat_tables")
-def create_stat_tables():
-    web_input_data = "/usr/src/input_data/"
-    cursor = connection.cursor()
-    for filename in os.listdir(web_input_data):
-        table_name=filename[:-4]
-        cursor.execute(f"DROP TABLE IF EXISTS {table_name};")
-    cursor.execute(open("utilities/stats_schema.sql", "r").read())
-    connection.commit()
+    def create_stat_tables(self):
+        web_input_data = "/usr/src/input_data/"
+        cursor = self.conn.cursor()
+        for filename in os.listdir(web_input_data):
+            table_name = filename[:-4]
+            cursor.execute(f"DROP TABLE IF EXISTS {table_name};")
+        cursor.execute(open("utilities/stats_schema.sql", "r").read())
+        self.conn.commit()
 
-
-
-@cli.command("populate_static_tables")
-def populate_static_tables():
-    psql_fixed_data = "/var/lib/world_factbook/fixed_data/"
-    # populate Region and Country tables
-    cursor = connection.cursor()
-    cursor.execute(
-        open("utilities/populate_country_region.sql", "r").read(),
-        (psql_fixed_data+"countries.csv", psql_fixed_data+"iso.csv")
-    )
-    connection.commit()
-
-
-
-@cli.command("populate_stat_tables")
-def populate_stat_tables():
-    # csv paths in psql container
-    psql_input_data = "/var/lib/world_factbook/input_data/"
-    # csv path in web container
-    web_input_data = "/usr/src/input_data/"
-    cursor = connection.cursor()
-    # populate country statistic tables
-    for filename in os.listdir(web_input_data):
+    def populate_static_tables(self):
+        psql_fixed_data = "/var/lib/world_factbook/fixed_data/"
+        cursor = self.conn.cursor()
         cursor.execute(
-            open("utilities/populate_stat_table.sql", "r").read().format(
-                table_name=filename[:-4],
-                csv=os.path.join(psql_input_data, filename)
-            )
+            open("utilities/populate_country_region.sql", "r").read(),
+            (psql_fixed_data + "countries.csv", psql_fixed_data + "iso.csv")
         )
-    connection.commit()
+        self.conn.commit()
 
+    def populate_stat_tables(self):
+        psql_input_data = "/var/lib/world_factbook/input_data/"
+        web_input_data = "/usr/src/input_data/"
+        cursor = self.conn.cursor()
+        for filename in os.listdir(web_input_data):
+            cursor.execute(
+                open("utilities/populate_stat_table.sql", "r").read().format(
+                    table_name=filename[:-4],
+                    csv=os.path.join(psql_input_data, filename)
+                )
+            )
+        self.conn.commit()
 
+    def create_recent_stat_tables(self):
+        web_input_data = "/usr/src/input_data/"
+        cursor = self.conn.cursor()
+        for filename in os.listdir(web_input_data):
+            table_name = filename[:-4]
+            cursor.execute(f"DROP TABLE IF EXISTS {table_name}_recent;")
+            cursor.execute(f"CREATE TABLE {table_name}_recent (LIKE {table_name} INCLUDING ALL);")
+        self.conn.commit()
 
-@cli.command("create_recent_stat_tables")
-def create_recent_stat_tables():
-    web_input_data = "/usr/src/input_data/"
-    cursor = connection.cursor()
-    for filename in os.listdir(web_input_data):
-        table_name=filename[:-4]
-        # delete country recent statistic table if exists
-        cursor.execute(f"DROP TABLE IF EXISTS {table_name}_recent;")
-        # create country recent statistic table
-        cursor.execute(f"CREATE TABLE {table_name}_recent (LIKE {table_name} INCLUDING ALL);")
-    connection.commit()
-
-
-
-@cli.command("populate_recent_stat_tables")
-def populate_recent_stat_tables():
-    web_input_data = "/usr/src/input_data/"
-    cursor = connection.cursor()
-    for filename in os.listdir(web_input_data):
-        cursor.execute(
-            open("utilities/populate_recent_stat_table.sql", "r").read().format(
+    def populate_recent_stat_tables(self):
+        web_input_data = "/usr/src/input_data/"
+        cursor = self.conn.cursor()
+        for filename in os.listdir(web_input_data):
+            cursor.execute(
+                open("utilities/populate_recent_stat_table.sql", "r").read().format(
                     table_name=filename[:-4]
                 )
-        )
-    connection.commit()
+            )
+        self.conn.commit()
 
+
+
+@cli.command("run_all_scripts_with_check")
+def run_all_scripts_with_check():
+    StatTableManager().run_all_scripts_with_check()
 
 if __name__ == "__main__":
     cli()
